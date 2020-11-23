be9b38a019ecd54fee4667fd2a4e8d1fc371ac21
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-11-24 01:02:34 +0300

    add favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './css/vendor/tailwind.vendor.css';
 import './css/style.css';
 import greetingModule from "./dom";
+import favIconImg from "./assets/favicon.png";
 
 const init = () => {
+  const favIcon = document.querySelector('href="assets/favicon.png"');
+  favIcon.setAttribute('href', favIconImg);
+  
   const gm = greetingModule();
   const mainContent = document.getElementById('content');
   mainContent.classList.add(...['flex', 'justify-center'])
